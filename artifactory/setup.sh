4afd79afeeb557b2821c045776b0cb12e5ec690d
Mateusz <mateusz.kurdziel1@gmail.com>
2023-03-08 19:49:41 +0000

    update

--- artifactory/setup.sh
+++ artifactory/setup.sh
@@ -1,4 +1,3 @@
-docker stop artifactory
-docker system prune -f
+docker rm artifactory -f
 docker run -d -p 8081-8082:8081-8082 --name artifactory -v /home/panda/artifactory:/artifactory_extra_conf docker.bintray.io/jfrog/artifactory-jcr:7.39.4
 curl -XPOST -vu admin:password http://192.168.44.44:8081/artifactory/ui/jcr/eula/accept
